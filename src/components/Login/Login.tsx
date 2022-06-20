293dc634f68570555a684a6673ef1f0793e1ac4a
ymerilazii <ymerilazii@gmail.com>
2022-06-20 17:44:16 +0200

    Used Formik Library

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Button, TextField } from "@mui/material";
+import { useLoginFormik } from "../../lib/hooks/useLoginFormik";
+
+export const Login = () => {
+  const formik = useLoginFormik({
+    onSubmit(values, formikHelpers) {
+      console.info("values", values);
+    },
+  });
+
+  return (
+    <div
+      style={{
+        margin: "50px",
+      }}
+    >
+      <form
+        onSubmit={formik.handleSubmit}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          rowGap: "20px",
+          width: "400px",
+        }}
+      >
+        <h1>Login </h1>
+        <TextField label="Username" />
+        <TextField
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+        />
+        <Button type="submit" variant="contained">
+          Log in{" "}
+        </Button>
+      </form>
+    </div>
+  );
+};
